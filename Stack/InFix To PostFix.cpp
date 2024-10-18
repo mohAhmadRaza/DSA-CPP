97c7a1443f5eb92e15d3eedb11e66e8fc4fada1a
AhmadRaza <158742870+mohAhmadRaza@users.noreply.github.com>
2024-10-18 15:32:59 +0500

    Update InFix To PostFix.cpp

--- Stack/InFix To PostFix.cpp
+++ Stack/InFix To PostFix.cpp
@@ -1,13 +1,15 @@
-#include<iostream>
-#include<stack>
-#include<map>
+#include <iostream>
+#include <stack>
+#include <map>
+#include <vector>
 using namespace std;
 
-
-string ConvertIfixToPostFix(string InfixExpression){
+string InFixConverision(string InFix)
+{
     map<char, int> precedence;
+    string PostFix = "";
+    vector<char> Operators = {'(', ')', '+', '-', '/', '*', '^'};
     stack<char> st;
-    string PostFixExpression = "";
 
     precedence['+'] = 1;
     precedence['-'] = 1;
@@ -15,54 +17,108 @@ string ConvertIfixToPostFix(string InfixExpression){
     precedence['/'] = 2;
     precedence['^'] = 3;
 
-    for (int i=0 ; i<InfixExpression.length() ; i++){
-        char currChar = InfixExpression[i];
+    for (int i = 0; i < InFix.length(); i++)
+    {
+        char currChar = InFix[i];
 
-        if (currChar != '+' && currChar != '-' && currChar != '*' && currChar != '/' && currChar != '^' ){
-            PostFixExpression += currChar;
+        if (find(Operators.begin(), Operators.end(), currChar) == Operators.end())
+        {
+            char topValue = st.top();
+            PostFix += topValue;
+            st.pop();
         }
-        else{
-            if (!st.empty() && precedence[st.top()] >= precedence[currChar] ){
-                while (!st.empty() && precedence[st.top()] >= precedence[currChar])
-                {
-                    char currOperator = st.top();
-                    PostFixExpression += currOperator;
-                    st.pop();
-                }
-                st.push(currChar);
-            }
-            else if ( currChar == '(' ){
-                st.push(currChar);
-            }
-            else if ( currChar == ')'){
-                while (!st.empty() && st.top() != ')'){
-                    char currOperator = st.top();
-                    PostFixExpression += currOperator;
-                    st.pop();
-                }
+        else if (currChar == '(')
+        {
+            st.push(currChar);
+        }
+
+        else if (currChar == ')')
+        {
+
+            while (st.top() != '(')
+            {
+                char topValue = st.top();
+                PostFix += topValue;
                 st.pop();
             }
-            else{
-                st.push(currChar);
+            st.pop();
+        }
+
+        else
+        {
+            while (!st.empty() && precedence[st.top()] >= precedence[currChar])
+            {
+                char topValue = st.top();
+                PostFix += topValue;
+                st.pop();
             }
 
+            st.push(currChar);
         }
     }
 
-    while (!st.empty()){
-        PostFixExpression += st.top();
+    while (!st.empty())
+    {
+        PostFix += st.top();
         st.pop();
     }
-
-    return PostFixExpression;
+    return PostFix;
 }
 
-int main(){
-    string InFixExpression = "A+B*C+D";
-    cout << "iiii" << endl;
-    string PostFixExpressin = ConvertIfixToPostFix(InFixExpression);
-    cout << "dddd" << endl;
-    cout << "Expression Converted : " << PostFixExpressin << endl;
+int PostFixEvaluation(string PostFix)
+{
+    stack<char> st;
+    vector<char> Operators = {'+', '-', '/', '*', '^'};
 
+    for (int i = 0; i < PostFix.length(); i++)
+    {   
+        char currChar = PostFix[i];
+        if (find(Operators.begin(), Operators.end(), currChar) == Operators.end())
+        {
+            st.push(currChar);
+        }
+        else
+        {
+            int Operand2 = st.top();
+            st.pop();
+            int Operand1 = st.top();
+            st.pop();
+
+            if (currChar == '+')
+            {
+                st.push(Operand1 + Operand2);
+            }
+            else if (currChar == '-')
+            {
+                st.push(Operand1 - Operand2);
+            }
+            else if (currChar == '/')
+            {
+                st.push(Operand1 / Operand2);
+            }
+            else if (currChar == '*')
+            {
+                st.push(Operand1 * Operand2);
+            }
+            else if (currChar == '^') 
+            { 
+                st.push(Operand1 ^ Operand2); 
+            }
+        }
+    }
+    int Val = st.top();
+    st.pop();
+    return Val;
+}
+
+
+int main()
+{
+    string InFixExpression = "(2-3/4)*(5/6-7)";
+
+    string PostFixExpression = InFixConverision(InFixExpression);
+
+    int EvaluatedAns = PostFixEvaluation(PostFixExpression);
+    cout << "Answer : " << EvaluatedAns << endl;
     return 0;
 }
